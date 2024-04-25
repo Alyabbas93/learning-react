ad25847f814d228596524ad9167e191fa1c1c4fa
Ali Abbas <154668026+Alyabbas93@users.noreply.github.com>
2024-04-25 06:55:54 -0700

    making

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Main from '../../components/MainData'
+import Header from '../../components/header'
+import Footer from '../../components/footer/footer'
+
+// import Header from '../../components/header'
+// import Footer from '../../components/footer/footer'
+
+
+
+
+export default function Home() {
+  return (
+    <div className='main-div'>
+   <Main/>
+
+    </div>
+  )
+}
